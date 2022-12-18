abacdda57f8d147a160db160312ac5cb40448822
Ganesh-GK <ganesh@sparkouttech.com>
2022-12-18 13:31:07 +0530

    stub-path-changes

--- src/App/Console/Commands/GenerateModelWithCustomFile.php
+++ src/App/Console/Commands/GenerateModelWithCustomFile.php
@@ -29,51 +29,17 @@ class GenerateModelWithCustomFile extends Command
      */
     public function handle()
     {
-        $name = $this->ask('What is your model name?');
+        $name = strtolower($this->ask('What is your model name?'));
 
         if ($this->confirm('Do you want to add controller?')) {
             $this->controller($name);
         }
 
-        if ($this->confirm('Do you want to add blade?')) {
-            $this->blade($name);
+        if ($this->confirm('Do you want to add view?')) {
+            $this->view($name);
         }
 
-        $this->model($name="Test");
-    }
-
-    protected function getStub($type)
-    {
-//        $filePath = __DIR__.'/../../../resources/stubs/'.$type.'.stub'; //another way get file
-//        $filePath = resource_path("stubs/$type.stub"); // after published
-        $filePath = base_path('vendor/crud-dev/crud-dev/src/resources/stubs/'.$type.'.stub');
-
-        if (file_exists(base_path("stubs/$type.stub"))) {
-            return file_get_contents(base_path("stubs/$type.stub"));
-        } else {
-            return file_get_contents($filePath);
-        }
-    }
-
-    protected function blade($name)
-    {
-        $bladeName = strtolower(Str::plural($name));
-        $controllerName = ucfirst($name);
-        $route = Str::plural(strtolower($name));
-        $modelTemplate = str_replace(
-            ['{{modelNamePluralLowerCase}}'],
-            [$bladeName],
-            $this->getStub('blade')
-        );
-        file_put_contents(resource_path("views/{$bladeName}.blade.php"), $modelTemplate);
-        File::append(
-            base_path('routes/web.php'),
-            str_replace(
-                'controllerName',
-                '',
-                'Route::get("' . $route . '",[App\Http\Controllers\controllerName' . $controllerName . 'Controller::class, "view"]);'
-            )
-        );
+        $this->model($name);
     }
 
     protected function model($name)
@@ -94,28 +60,66 @@ class GenerateModelWithCustomFile extends Command
         $controllerName = ucfirst($name);
         $controllerTemplate = str_replace(
             [
-                '{{modelName}}',
-                '{{modelNamePluralLowerCase}}',
-                '{{modelNameSingularLowerCase}}'
+                '{{ namespace }}',
+                '{{ rootNamespace }}',
+                '{{ class }}'
             ],
             [
-                $controllerName,
-                strtolower(Str::plural($name)),
-                strtolower($name)
+                "App\Http\Controllers",
+                "App\\",
+                $controllerName."Controller"
             ],
             $this->getStub('controller')
         );
 
-        $route = Str::plural(strtolower($name));
-        File::append(
-            base_path('routes/api.php'),
-            str_replace(
-                'controllerName',
-                '',
-                'Route::resource("' . $route . '",App\Http\Controllers\controllerName' . $controllerName . 'Controller::class);'
-            )
-        );
+//        $route = Str::plural(strtolower($name));
+//        File::append(
+//            base_path('routes/api.php'),
+//            str_replace(
+//                'controllerName',
+//                '',
+//                'Route::resource("' . $route . '",App\Http\Controllers\controllerName' . $controllerName . 'Controller::class);'
+//            )
+//        );
 
         file_put_contents(app_path("/Http/Controllers/{$controllerName}Controller.php"), $controllerTemplate);
+    }
+
+    protected function view($name)
+    {
+        $bladeName = strtolower(Str::plural($name));
+
+        $modelTemplate = str_replace(
+            ['{{ class }}'],
+            [$bladeName],
+            $this->getStub('view')
+        );
+        file_put_contents(resource_path("views/{$bladeName}.blade.php"), $modelTemplate);
+
+//        $controllerName = ucfirst($name);
+//        $route = Str::plural(strtolower($name));
+//        File::append(
+//            base_path('routes/web.php'),
+//            str_replace(
+//                'controllerName',
+//                '',
+//                'Route::get("' . $route . '",[App\Http\Controllers\controllerName' . $controllerName . 'Controller::class, "view"]);'
+//            )
+//        );
+    }
+
+    protected function getStub($type)
+    {
+//        $filePath = __DIR__.'/../../../resources/stubs/'.$type.'.stub'; //another way get file
+//        $filePath = resource_path("views/vendor/crud-dev/$type.stub"); // after published
+//        $filePath = file_exists(base_path("stubs/$type.stub")); // default stub path
+
+        $filePath = base_path('vendor/crud-dev/crud-dev/src/resources/stubs/'.$type.'.stub');
+
+        if (file_exists(resource_path("views/vendor/crud-dev/$type.stub"))) {
+            return file_get_contents(resource_path("views/vendor/crud-dev/$type.stub"));
+        } else {
+            return file_get_contents($filePath);
+        }
     }
 }
